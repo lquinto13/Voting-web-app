0bcb7b419957d35b90972f7b05f19094dcffd9bc
Lance_Gabriel Quinto <lancegabriel.quinto@synpulse.com>
2023-11-29 20:02:11 +0800

    push everything

---
+++ Voting App/script.js
@@ -0,0 +1,16 @@
+let tallyTotal = 0; 
+const javaTally = document.getElementById("java");
+const pythonTalluy = document.getElementById("python");
+const javascriptTally = document.getElementById("javascript");
+
+
+function submitVote(){
+    tally +=1;
+    
+    
+}
+
+function toPercent(vote){
+    let votePercent = vote/100 
+    return votePercent
+}
